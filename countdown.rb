7f8dae08a52eb39711ad71fef506170a859d6fdb
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-26 00:56:16 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,14 @@
 
 def countdown(integer)
   integer = 10
+  integer -= 1
   while (integer < 10)
-    integer -= 1
     puts "#{integer} SECOND(S)!"
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer)
+  
+end
+
